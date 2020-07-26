450c61dff22fecc03ed3284af3f652ad1f690f31
Bartosz Pietraszek <bartosz.pietraszek92@gmail.com>
2020-07-26 13:55:36 +0200

    #26 Dodać widok elementu listy 'Kategorie marzeń'

---
+++ app/src/main/java/com/example/alpaki/presentation/adapters/DreamCategoriesAdapter.kt
@@ -0,0 +1,92 @@
+package com.example.alpaki.presentation.adapters
+
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.alpaki.R
+import com.example.alpaki.databinding.ItemDesktopCategoryBinding
+import com.example.alpaki.presentation.adapters.DreamCategoriesAdapter.DreamCategoryViewHolder
+import com.example.domain.models.DreamCategory
+import com.example.domain.models.DreamCategory.*
+
+class DreamCategoriesAdapter(
+    private val onItemClick: (DreamCategory) -> Unit
+) : RecyclerView.Adapter<DreamCategoryViewHolder>() {
+
+    private val categories: List<DreamCategory> = listOf(
+        VISIT,
+        GIFT,
+        MEET,
+        BECOME,
+        GIVE,
+        QUICK_MODE
+    )
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DreamCategoryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemDesktopCategoryBinding.inflate(inflater, parent, false)
+        return DreamCategoryViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = categories.size
+
+    override fun onBindViewHolder(holder: DreamCategoryViewHolder, position: Int) {
+        holder.bind(categories[position])
+    }
+
+    inner class DreamCategoryViewHolder(
+        private val binding: ItemDesktopCategoryBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        private val resources: Resources
+            get() = binding.root.context.resources
+
+        fun bind(item: DreamCategory) {
+            val itemResources = matchResources(item)
+            binding.textItemDesktopCategory.text = itemResources.first
+            binding.imageItemDesktopCategory.setImageDrawable(itemResources.second)
+            val labelTextColor: Int =
+                if (item == QUICK_MODE) resources.getColor(R.color.burnt_sienna)
+                else resources.getColor(R.color.dimGray)
+            binding.textItemDesktopCategory.setTextColor(labelTextColor)
+            binding.root.setOnClickListener { onItemClick(item) }
+        }
+
+        private fun matchResources(category: DreamCategory): Pair<String, Drawable> {
+            val label: String
+            val icon: Drawable
+            when (category) {
+                VISIT -> {
+                    label = resources.getString(R.string.text_desktop_category_visit)
+                    icon = resources.getDrawable(R.drawable.ic_dream_category_visit_dim_gray, null)
+                }
+                GIFT -> {
+                    label = resources.getString(R.string.text_desktop_category_gift)
+                    icon = resources.getDrawable(R.drawable.ic_dream_category_gift_dim_gray, null)
+                }
+                MEET -> {
+                    label = resources.getString(R.string.text_desktop_category_meet)
+                    icon = resources.getDrawable(R.drawable.ic_dream_category_meet_dim_gray, null)
+                }
+                BECOME -> {
+                    label = resources.getString(R.string.text_desktop_category_become)
+                    icon = resources.getDrawable(R.drawable.ic_dream_category_become_dim_gray, null)
+                }
+                GIVE -> {
+                    label = resources.getString(R.string.text_desktop_category_give)
+                    icon = resources.getDrawable(R.drawable.ic_dream_category_give_dim_gray, null)
+                }
+                QUICK_MODE -> {
+                    label = resources.getString(R.string.text_desktop_category_quick_mode)
+                    icon = resources.getDrawable(
+                        R.drawable.ic_dream_category_quick_mode_burnt_sienna,
+                        null
+                    )
+                }
+            }
+            return label to icon
+        }
+    }
+}
---
+++ domain/src/main/java/com/example/domain/models/DreamCategory.kt
@@ -0,0 +1,11 @@
+package com.example.domain.models
+
+enum class DreamCategory {
+
+    VISIT,
+    GIFT,
+    MEET,
+    BECOME,
+    GIVE,
+    QUICK_MODE
+}
