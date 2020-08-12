5559b56d902d9dc013dc55bbfc38fae6547ecf78
AdamCzajkowski <adam.czajkowski8@gmail.com>
2020-08-12 14:00:20 +0200

    fixed viewModel

--- app/src/main/java/com/example/alpaki/presentation/login/ResetPasswordFragment.kt
+++ app/src/main/java/com/example/alpaki/presentation/login/ResetPasswordFragment.kt
@@ -1,17 +1,25 @@
 package com.example.alpaki.presentation.login
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
 import com.example.alpaki.R
 import com.example.alpaki.core.views.base.BaseFragment
-import com.example.alpaki.databinding.FragmentRegisterBinding
-import com.example.alpaki.presentation.fragments.RegisterViewModel
+import com.example.alpaki.databinding.FragmentResetPasswordBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_register.*
 
 @AndroidEntryPoint
-class ResetPasswordFragment : BaseFragment<FragmentRegisterBinding>() {
+class ResetPasswordFragment : BaseFragment<FragmentResetPasswordBinding>() {
 
     override val layoutId: Int = R.layout.fragment_reset_password
 
-    private val resetPasswordViewModel: RegisterViewModel by viewModels()
+    private val resetPasswordViewModel: ResetPasswordViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = resetPasswordViewModel
+
+        titleBarView.setLeftIconClickListener { requireActivity().onBackPressed() }
+    }
 }
