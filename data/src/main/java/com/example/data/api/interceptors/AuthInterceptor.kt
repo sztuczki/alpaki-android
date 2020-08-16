818b8124910ac039a1d13c555034cb2f07dbf564
BartoszP <bartosz.pietraszek92@gmail.com>
2020-08-16 12:41:37 +0200

    #50 Dodać interceptor z headerem do autoryzacji

---
+++ data/src/main/java/com/example/data/api/interceptors/AuthInterceptor.kt
@@ -0,0 +1,21 @@
+package com.example.data.api.interceptors
+
+import com.example.data.api.HEADER_AUTHORIZATION
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthInterceptor @Inject constructor() : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var request = chain.request()
+//        TODO: replace Admin token with individual Users tokens when available
+        request = request.newBuilder()
+            .addHeader(
+                HEADER_AUTHORIZATION,
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxIiwidW5pcXVlX25hbWUiOiIxIiwicm9sZSI6IjciLCJuYmYiOjE1OTc0MjAyNTAsImV4cCI6MTYyODk0OTA1MCwiaWF0IjoxNTk3NDIwMjUwfQ.8s5JNh7gEWGIlOOZpjIl6CV5m2_JWg8AdgzQlCWvr5I"
+            )
+            .build()
+        return chain.proceed(request)
+    }
+}
