6e8bd039622f2badcdf97ba8cf38beddfef46a9b
Michał Ambroży <ambrozy@talixo.de>
2020-08-24 14:40:26 +0200

    Create TasksFragment Layout

--- app/src/main/java/com/example/alpaki/presentation/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/alpaki/presentation/tasks/TasksFragment.kt
@@ -1,10 +1,28 @@
 package com.example.alpaki.presentation.tasks
 
+import android.os.Bundle
+import android.view.View
+import com.example.alpaki.DreamModel
 import com.example.alpaki.R
 import com.example.alpaki.core.views.base.BaseFragment
+import com.example.alpaki.databinding.ActiveDreamCardViewBinding
 import com.example.alpaki.databinding.FragmentTasksBinding
+import kotlinx.android.synthetic.main.fragment_tasks.*
 
 class TasksFragment : BaseFragment<FragmentTasksBinding>() {
-
+    private val testImageUrl = "https://images.app.goo.gl/GeAiwEuyYp7S3fX49"
     override val layoutId: Int = R.layout.fragment_tasks
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.dream = DreamModel(
+            0,
+            "Chcę dostać",
+            "Różowy laptop",
+            "Gosia",
+            8,
+            "Warszawa",
+            10,
+            testImageUrl
+        )
+    }
 }
